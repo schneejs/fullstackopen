be53692e333029de962e0e3316fc0c42d7b656fa
ArcSineSoftware <>
2020-04-18 01:20:58 +0600

    Last exercises from part2 have been done.

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,6 +1,18 @@
 import React, {useState, useEffect} from "react";
 import Communication from "./Communication";
 
+const Notification = ({lastReport}) => {
+	if (lastReport) {
+		const className = lastReport.success ? "sucnotif" : "errnotif";
+		return (
+			<div>
+				<p className={className}>{lastReport.text}</p>
+			</div>
+		);
+	} else
+		return (<></>);
+}
+
 const AddPerson = ({
 		newName,
 		newPhone,
@@ -39,7 +51,7 @@ const Contacts = ({filteredPersons, handleFilterChange, removePerson}) => (
 			{filteredPersons.map(person =>
 				<li key={person.id}>
 					{person.name} {person.number} 
-					<button type="button" onClick={() =>
+					<button className="rem" type="button" onClick={() =>
 						Communication
 							.deletePerson(person.id)
 							.then(({data}) =>
@@ -59,26 +71,46 @@ const Contacts = ({filteredPersons, handleFilterChange, removePerson}) => (
 );
 
 const App = () => {
+	// Notification system
+	const [lastReport, setLastReport] = useState(null);
+	// Contacts that are going to be fetched from a database
 	const [persons, setPersons] = useState([]);
-	const removePerson = personId =>
+	// Remove a contact by its ID (not index!)
+	const removePerson = personId => {
 		setPersons(persons.filter(person => person.id !== personId));
+		setLastReport({
+			success: true,
+			text: `The person has been successfully deleted.`
+		});
+	}
+	// Persons filtered by filter string enter by the user
+	// automatically updated when persons update
 	const [filteredPersons, setFilteredPersons] = useState([]);
+	// Text fields
 	const [newName, setNewName] = useState('');
 	const [newPhone, setNewPhone] = useState('');
 	const clearForm = () => {
 		setNewName('');
 		setNewPhone('');
 	};
+	// Filter field
 	const [currentFilter, setCurrentFilter] = useState('');
 	const is_matching_filter = filterStr => ({name}) =>
 		name.toLowerCase().includes(filterStr.toLowerCase());
 	
+	// Load the database
 	useEffect(() => {
 		Communication
 			.loadPersons()
 			.then(({data}) => setPersons(data));
 	}, []);
 	
+	// Reports handler
+	useEffect(() => {
+		setTimeout(() => setLastReport(null), 2000);
+	}, [lastReport]);
+	
+	// Updater of filteredPersons
 	useEffect(() => {
 		if (currentFilter) {
 			const newFilteredPersons =
@@ -88,6 +120,7 @@ const App = () => {
 			setFilteredPersons(persons);
 	}, [persons, currentFilter]);
 	
+	// Functions for handling field changes
 	const handleNameChange = event =>
 		setNewName(event.target.value);
 	const handlePhoneChange = event =>
@@ -95,16 +128,17 @@ const App = () => {
 	const handleFilterChange = event =>
 		setCurrentFilter(event.target.value);
 	
+	// Submit button clicking callback
 	const handleSubmitClick = event => {
 		event.preventDefault();
+		// Index of a contact that the user is searching for
 		const indexOfNewName = persons
 			.map(person => person.name)
 			.indexOf(newName);
-		if (indexOfNewName >= 0) {
+		if (indexOfNewName >= 0) { // that name already exists
 			const confirmResult =
 				window.confirm(`Are you sure you want to change the number of ${newName}?`);
-			if (confirmResult) {
-				// if pressed yes
+			if (confirmResult) // Yes was pressed
 				Communication
 					.updatePerson({
 						id: persons[indexOfNewName].id,
@@ -115,21 +149,38 @@ const App = () => {
 						let newPersons = [...persons];
 						newPersons[indexOfNewName] = data;
 						setPersons(newPersons);
+						setLastReport({
+							success: true,
+							text: "The number has been changed!"
+						});
+					})
+					.catch(error => {
+						setLastReport({
+							success: false,
+							text: "The contact has been deleted already."
+						});
 					});
-			} else // cancelled
+			else // cancelled
 				clearForm();
 		}
-		else if (persons.map(
-			person => person.phone).indexOf(newPhone) >= 0
-		)
-			alert(`${newPhone} is already in the phonebook!`);
+		else if (persons.map(p => p.number).indexOf(newPhone) >= 0)
+			setLastReport({
+				success: false,
+				text: `${newPhone} is already in the phonebook!`
+			});
 		else if (newName) {
 			Communication
 				.uploadPerson({
 					name: newName,
 					number: newPhone
 				})
-				.then(({data}) => setPersons(persons.concat(data)));			
+				.then(({data}) => {
+					setPersons(persons.concat(data));
+					setLastReport({
+						success: true,
+						text: `Added ${data.name}`
+					});
+				});			
 		}
 		clearForm();
 	};
@@ -137,6 +188,7 @@ const App = () => {
 	return (
 		<div>
 			<h2>Phonebook</h2>
+			<Notification lastReport={lastReport} />
 			<AddPerson
 				newName={newName}
 				newPhone={newPhone}

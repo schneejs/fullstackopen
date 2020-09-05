0863c683b6400e7a0f0e2c69d0f363aead70865f
schneejs <13682559+schneejs@users.noreply.github.com>
2020-09-06 02:12:34 +0600

    8.16

--- part8/library-backend/library_backend.js
+++ part8/library-backend/library_backend.js
@@ -1,11 +1,23 @@
 const { ApolloServer, gql, UserInputError, ApolloError } = require('apollo-server')
+const jwt = require('jsonwebtoken')
 const mongoose = require('mongoose')
 const Book = require('./book')
 const Author = require('./author')
 
+const JWT_SECRET = 'sekret'
 const MONGODB_URI = 'mongodb+srv://fullstack:TU68qmUv7sDzr93@cluster0-udxk6.mongodb.net/booklist?retryWrites=true&w=majority'
 
 const typeDefs = gql`
+    type User {
+        username: String!
+        favoriteGenre: String
+        id: ID!
+    }
+
+    type Token {
+        value: String!
+    }
+
     type Author {
         name: String!
         id: ID!
@@ -32,9 +44,18 @@ const typeDefs = gql`
             name: String!,
             setBornTo: Int
         ): Author
+        createUser(
+            username: String!
+            favoriteGenre: String!
+        ): User
+        login(
+            username: String!
+            password: String!
+        ): Token
     }
 
     type Query {
+        me: User
         bookCount: Int!
         allBooks(author: String, genre: String): [Book!]!
         authorCount: Int!
@@ -45,7 +66,7 @@ const typeDefs = gql`
 const resolvers = {
     Mutation: {
         addBook: async (_, book) => {
-            let author 
+            let author
             try {
                 author = await Author.findOne({ name: book.author })
             } catch {
@@ -94,9 +115,18 @@ const resolvers = {
                 }
             }
             return author
+        },
+        createUser: async (_, { username, favoriteGenre }) => {
+            return { username, favoriteGenre }
+        },
+        login: async (_, { username, password }) => {
+            if (password !== 'secret')
+                throw new UserInputError('Wrong password')
+            return { value: jwt.sign({ username }, JWT_SECRET) }
         }
     },
     Query: {
+        me: async (_, _2, context) => context.currentUser,
         bookCount: async () => await Book.count({}),
         allBooks: async (_, { author, genre }) => {
             const filter = {}
@@ -120,7 +150,16 @@ const resolvers = {
 const server = new ApolloServer({
     typeDefs,
     resolvers,
-    cors: true
+    cors: true,
+    context: async ({ req }) => {
+        const auth = req ? req.headers.authorization : null
+        if (auth && auth.toLowerCase().startsWith('bearer')) {
+            const token = auth.substring(7)
+            const decodedToken = jwt.verify(token, JWT_SECRET)
+            if (decodedToken)
+                return { currentUser: { username: decodedToken.username } }
+        }
+    }
 })
 
 const main = async () => {
